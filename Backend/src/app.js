8eaae129858f1cc0201df23378d2ff6afb1f5bec
osama <osamagamal1611@gmail.com>
2026-05-01 02:12:46 +0300

    fix vercle and db files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,11 +11,7 @@ import adminRouter from "./modules/admin/users/user.routes.js";
 import adminOrderRouter from "./modules/admin/orders/orders.routes.js";
 import adminProductRouter from "./modules/admin/products/prooducts.routes.js";
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 export const connectDB = async (uri) => {
-  if (isConnected) return;
   try {
     await mongoose.connect(uri);
-    isConnected = true;
     console.log("DB connected 🔥");
   } catch (err) {
-    console.error("DB Connection Failed:", err.message);
+    console.error(err);
+    process.exit(1); 
   }
 };
