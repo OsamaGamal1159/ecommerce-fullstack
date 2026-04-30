eeaf7efd3f4a4a15444800ab1d817959d5b3f3d3
osama <osamagamal1611@gmail.com>
2026-04-30 15:42:52 +0300

    fix cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,13 @@ import adminRouter from "./modules/admin/users/user.routes.js";
 import adminOrderRouter from "./modules/admin/orders/orders.routes.js";
 import adminProductRouter from "./modules/admin/products/prooducts.routes.js";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://ecommerce-fullstack-h93n6vjkx-osamagamal1611-3045s-projects.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
